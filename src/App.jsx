6e6a8cea0b67a85417ff244074eb326484a5f79f
nairx <nairxprv@gmail.com>
2025-07-10 11:03:33 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Register from "./components/Register";
 import "./App.css";
-import Home from "./components/Home.jsx";
 function App() {
   return (
     <div className="App-Container">
       <h1 style={{ backgroundColor: "orange" }}>MERN Frontend</h1>
-      <Home age={21} />
+      <Register/>
       <h3>This is footer</h3>
     </div>
   );
