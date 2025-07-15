44580a15465bae4d8872beed31797185703f3f69
nairx <nairxprv@gmail.com>
2025-07-15 10:45:50 +0530

    updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,5 +3,26 @@ import { useContext } from "react";
 import { AppContext } from "../App";
 export default function Cart() {
   const { user, cart, setCart } = useContext(AppContext);
-  return <div>{cart && cart.map((value) => <li>{value.productName}</li>)}</div>;
+  const increment = (id, qty) => {
+
+  };
+
+  const decrement = (id, qty) => {
+    
+  }
+  return (
+    <div>
+      <h2>My Cart</h2>
+      {cart &&
+        cart.map((value) => (
+          <li key={value._id}>
+            {value.productName}-{value.price}-
+            <button onClick={() => decrement(value._id.qty)}>-</button>
+            {value.qty}
+            <button onClick={() => increment(value._id, qty)}>+</button>-
+            {value.price * value.qty}
+          </li>
+        ))}
+    </div>
+  );
 }
