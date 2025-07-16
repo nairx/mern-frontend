8720e34e1d46ea9fd480d9471b0957fb831d37ca
nairx <nairxprv@gmail.com>
2025-07-16 15:58:19 +0530

    updated

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -6,17 +6,22 @@ import { useFetcher } from "react-router-dom";
 export default function Orders() {
   const [orders, setOrders] = useState([]);
   const [error, setError] = useState();
+  const [page, setPage] = useState(1);
+  const [limit,setLimit]= useState(3)
+  const [totalPages, setTotalPages] = useState(1);
+  const [status, setStatus] = useState("Pending");
   const { user } = useContext(AppContext);
   const API_URL = import.meta.env.VITE_API_URL;
   const fetchOrders = async () => {
     try {
-      const url = `${API_URL}/api/orders`;
+      const url = `${API_URL}/api/orders/?page=${page}&limit=${limit}&status=${status}`;
       const result = await axios.get(url, {
         headers: {
           Authorization: `Bearer ${user.token}`,
         },
       });
-      setOrders(result.data);
+      setOrders(result.data.orders);
+      setTotalPages(result.data.total);
     } catch (err) {
       console.log(err);
       setError("Something went wrong");
@@ -24,7 +29,7 @@ export default function Orders() {
   };
   useEffect(() => {
     fetchOrders();
-  }, []);
+  }, [status]);
   const updateOrder = async (status, id) => {
     try {
       const url = `${API_URL}/api/orders/${id}`;
@@ -39,12 +44,15 @@ export default function Orders() {
     <div>
       <h2>Order Management</h2>
       <div>
-        <select>
-          <option value="Pending">Pending</option>
-          <option value="Completed">Completed</option>
-          <option value="Cancelled">Cancelled</option>
+        <select defaultValue="Pending" onChange={(e) => setStatus(e.target.value)}>
+          <option value="">All</option>
+          <option value="Pending" >
+            Pending
+          </option>
+          <option value="completed">Completed</option>
+          <option value="cancelled">Cancelled</option>
         </select>
-        <button>Show</button>
+        {/* <button>Show</button> */}
       </div>
       {orders &&
         orders.map((order) => (
