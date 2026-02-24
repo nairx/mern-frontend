c943b8b4ff5e0c5479f682ba86cd43d8572e5188
nairx <nairxprv@gmail.com>
2026-02-24 16:55:24 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
 
+  const fetchProducts = async () => {
+    const res = await axios.get("http://localhost:8080/api/products");
+    setProducts(res.data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {products.map((product) => (
+        <li>{product.name}</li>
+      ))}
+    </div>
+  );
 }
-
-export default App
+export default App;
